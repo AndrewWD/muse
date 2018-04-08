14f38a9f398b819820ab14dbd6dc78e6e8b3f8ac
WaterDragon <ygaolucky@gmail.com>
2018-04-08 21:01:11 +0000

    Connect to mongoDB with mangoose

--- app.js
+++ app.js
@@ -1,38 +1,48 @@
-let express = require('express');
-let app = express();
-let bodyparser = require('body-parser');
+const express = require('express'),
+      app = express(),
+      bodyparser = require('body-parser'),
+      mongoose = require('mongoose');
 
 app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost/museums');
 
-let campgrounds = [
-        {name: 'Nam Shan Campsite', image: 'https://media.timeout.com/images/103933543/750/562/image.jpg'},
-        {name: 'Tap Mun Campsite', image: 'https://media.timeout.com/images/103819969/750/562/image.jpg'},
-        {name: 'Hok Tau Campsite', image: 'https://media.timeout.com/images/103931927/750/562/image.jpg'},
-        {name: 'Nam Shan Campsite', image: 'https://media.timeout.com/images/103933543/750/562/image.jpg'},
-        {name: 'Tap Mun Campsite', image: 'https://media.timeout.com/images/103819969/750/562/image.jpg'},
-        {name: 'Hok Tau Campsite', image: 'https://media.timeout.com/images/103931927/750/562/image.jpg'},
-        {name: 'Nam Shan Campsite', image: 'https://media.timeout.com/images/103933543/750/562/image.jpg'},
-        {name: 'Tap Mun Campsite', image: 'https://media.timeout.com/images/103819969/750/562/image.jpg'},
-        {name: 'Hok Tau Campsite', image: 'https://media.timeout.com/images/103931927/750/562/image.jpg'}
-];
+
+const museumSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+const museum = mongoose.model('museum', museumSchema);
 
 app.get('/', (req, res)=>{
     res.render('landing');
 });
 
-app.get('/campgrounds', (req, res)=>{
-    res.render('campgrounds', {campgrounds: campgrounds});
+app.get('/museums', (req, res)=>{
+    museum.find({}, (err, museums)=>{
+        if(err){
+            console.log(err);
+        } else{
+            res.render('museums', {museums: museums});
+        }
+    })
 });
 
-app.post('/campgrounds', (req, res)=>{
-    var name = req.body.name;
-    var image = req.body.image;
-    campgrounds.push({name: name, image: image});
-    res.redirect('/campgrounds'); 
+app.post('/museums', (req, res)=>{
+    let name = req.body.name;
+    let image = req.body.image;
+    const newMuseum = {name: name, image: image};
+    museum.create(newMuseum, (err, museums)=>{
+        if(err){
+            console.log(err);
+        } else{
+          res.redirect('/museums'); 
+        }
+    });
 });
 
-app.get('/campgrounds/new', (req, res)=>{
+app.get('/museums/new', (req, res)=>{
     res.render('new');
 });
 app.listen(process.env.PORT, process.env.IP, ()=>{
