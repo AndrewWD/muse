ef0c44e1ff7917589a9a858e48778b7d564acae5
WaterDragon <ygaolucky@gmail.com>
2018-04-08 22:32:54 +0000

    Add a show page

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ mongoose.connect('mongodb://localhost/museums');
 
 const museumSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 const museum = mongoose.model('museum', museumSchema);
@@ -32,7 +33,8 @@ app.get('/museums', (req, res)=>{
 app.post('/museums', (req, res)=>{
     let name = req.body.name;
     let image = req.body.image;
-    const newMuseum = {name: name, image: image};
+    let desc = req.body.description;
+    const newMuseum = {name: name, image: image, description: desc};
     museum.create(newMuseum, (err, museums)=>{
         if(err){
             console.log(err);
@@ -44,6 +46,16 @@ app.post('/museums', (req, res)=>{
 
 app.get('/museums/new', (req, res)=>{
     res.render('new');
+});
+
+app.get('/museums/show/:id', (req, res)=>{
+    museum.findById(req.params.id, (err, museum)=>{
+        if(err){
+            console.log(err);
+        } else{
+            res.render('show', {museum: museum});
+        }
+    });
 });
 app.listen(process.env.PORT, process.env.IP, ()=>{
     console.log('The server has started!');
