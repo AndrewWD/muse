e4d570c4a2e1bb35db02f30f2a68419fec9db817
WaterDragon <ygaolucky@gmail.com>
2018-04-29 22:44:16 -0400

    Add price function

--- models/museum.js
+++ models/museum.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const museumSchema = new mongoose.Schema({
     name: String,
+    price: String,
     image: String,
     description: String,
     author: {
